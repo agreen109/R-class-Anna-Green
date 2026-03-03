9789bff865f3c60346bddb9efc344146d5dfa15f
agreen109 <agree109@kent.edu>
2026-03-03 12:54:05 -0500

    Almost done with lab 2

--- Lab 2/Lab 2.R
+++ Lab 2/Lab 2.R
@@ -57,43 +57,69 @@ dam_dataset <- dams %>%
 
 ggplot(dam_dataset, aes(x = YEAR, y = STATE)) +
   geom_point(color = "darkblue") +
-  labs(title = "Scatter plot of dam dataset") #why is it only filtering VA and PA? ----
+  labs(title = "Scatter plot of dam dataset") 
 
 
-#1.5 (NEED TO DO) one last aspatial visualization
+#1.5 (NEED TO DO) one last aspatial visualization ----
 
-glimpse(streams) #2.1 (NEED TO FIX) 5 longest streams
-longest_streams <- streams %>%
-  mutate(length = st_length(.)) %>%
+glimpse(streams) #2.1 five longest streams (not sure if this is correct output) ----
+streams_length <- streams %>%
+  mutate(length = st_length(geometry)) 
+
+top_5_streams <- streams_length %>%
   arrange(desc(LengthKM)) %>%
   slice(1:5)
+print(top_5_streams)
 
-glimpse(longest_streams)
 
 glimpse(streams)
 glimpse(counties)
-county_streams <- st_join(streams, counties) %>% #2.2 three counties with greatest total length ----
+county_streams <- st_join(streams, counties) %>% #2.2 three counties with greatest total length (in m) ----
   group_by(STATEFP10) %>%
   summarise(total_length = sum(st_length(.), na.rm = TRUE)) %>%
   arrange(desc(total_length)) %>%
   slice(1:3)
-glimpse(county_streams)
+print(county_streams)
 
 
-glimpse(bmps)
-county_costs <- bmps %>% 
-  group_by(County_FIPS) %>% 
-  summarise(total_bmp_cost = sum(Cost, na.rm = TRUE))
+glimpse(bmps) #2.3 (NEED TO FIX) ----
+glimpse(counties)
+county_costs <- bmps %>%
+  group_by(Geography) %>% 
+  summarize(Total_Cost = sum(Cost, na.rm = TRUE))
 
 county_map_data <- counties %>%
-  left_join(county_costs, by = "County_FIPS")
+  left_join(county_costs, by = c("NAME10" = "Cost"))
 
-ggplot(county_map_data) +
-  geom_sf(aes(fill = total_bmp_cost)) +
-  scale_fill_viridis_c(label = scales::dollar) +
-  labs(title = "Total BMP Cost by County")
+ggplot(data = county_map_data) +
+  geom_sf(aes(fill = Total_Cost), color = "blue", size=0.4) +
+  scale_fill_viridis_c(labels = scales::dollar, option = "magma", na.value = "pink") +
+  theme_minimal()+
+  labs(
+    title = "Total BMP Cost by County",
+    fill = "Total_Cost"
+  )
+
+tm_shape(county_map_data)+tm_polygons(fill="Cost")
 
 
 
+glimpse(streams) #2.4 for each dam, closest stream segment ----
+glimpse(dams)
+closest_stream <- st_nearest_feature(dams, streams)
 
+dams_spatial <- dams %>%
+  mutate(Closest_Stream_ID = streams$OBJECTID_1[closest_stream])
+
+print(dams_spatial)
+
+
+
+dams_per_state_summary <- dams_spatial %>% #2.5 how many removed dams in each state ----
+  st_drop_geometry() %>% 
+  group_by(STATE) %>%
+  tally(name = "Total_Removed_Dams") %>%
+  arrange(desc(Total_Removed_Dams))
+
+print(dams_per_state_summary)
 
