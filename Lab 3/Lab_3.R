bd470895310066a2d74ef24566cd2fd5383a8c3e
agreen109 <agree109@kent.edu>
2026-03-24 12:06:39 -0400

    big progress made to lab 3!

--- Lab 3/Lab_3.R
+++ Lab 3/Lab_3.R
@@ -1,3 +1,5 @@
+
+#working through prelab data ----
 install.packages("tidycensus")
 library(tidycensus)
 library(tidyverse)
@@ -53,3 +55,64 @@ F75.lag<-lag.listw(lw,ohio.projected$B01001_047E)
 F75.lag
 moran.test(ohio.projected$B01001_047E,lw)
 moran.plot(ohio.projected$B01001_047E,lw,zero.policy=TRUE,plot=TRUE)
+
+#starting lab 3 work ----
+
+fivestates <- c("Ohio", "Pennsylvania", "West Virginia", 
+                     "Indiana", "Michigan") #1, using 5 states ----
+fiveSS <- us_states %>%
+  filter(NAME %in% fivestates) %>%
+  st_transform(crs = 5070)
+glimpse(fiveSS)
+
+
+fiveSS <- fiveSS %>% #2, total population variable ----
+  mutate(
+    area_sqkm = as.numeric(st_area(.)) / 1e6, 
+    pop_density = total_pop_15 / area_sqkm
+  )
+glimpse(fiveSS)
+
+
+hist(fiveSS$pop_density, #3, histogram of population density ----
+     main = "Histogram of population density",
+     xlab = "population density (people per km^2)",
+     col = "lightgreen")
+    
+
+tm_shape(fiveSS) +    #4, chloropleth make of population density ----
+  tm_polygons(col = "pop_density",
+              title = "population density (per km^2)",
+              palette = "Greens") +
+  tm_layout(main.title = "Choropleth map of population density")
+
+
+queen <- poly2nb(fiveSS, queen = TRUE) #picking queen for question 5 ----
+
+
+queenlistw <- nb2listw(queen, style = "W", zero.policy = TRUE) #5.1 row standardize the W ----
+
+
+numneighbors <- card(queen) #5.2 plot a histogram of number of neighbors ----
+hist(numneighbors,
+     main = "Number of neighbors (using queen)",
+     xlab = "Number of neighbors",
+     col = "darkgreen")
+
+
+averageneighbors <- mean(numneighbors) #5.3 average number of neighbors ----
+cat("Average number of neighbors (Queen):", round(avgerageneighbors, 2), "\n")
+glimpse(averageneighbors)
+
+
+moran.plot(fiveSS$pop_density, #5.4 make a moran plot ----
+           listw = queenlistw,
+           main = "Moran plot using queen",
+           xlab = "Population density",
+           ylab = "Spatially lag population density")
+
+
+
+
+
+
